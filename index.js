7e0594812b2ce718ba9de385998db39321219353
Berlin <workatlaravel@gmail.com>
2024-11-22 17:02:23 +0530

    trust proxy added

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.set("trust proxy", 1);
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "fallback-secret",
